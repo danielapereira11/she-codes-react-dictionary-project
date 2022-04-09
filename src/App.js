fced72274f2643d4144f6e2c308873fa5ef2e3a7
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2022-04-09 15:26:00 +0100

    Added Bootstrap
    Used addition of link rel="stylesheet" instead of importing Bootstrap with npm;
    Added button to test if working.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">This is HEADER</header>
+      <div className="btn btn-primary">Button</div>
     </div>
   );
 }
